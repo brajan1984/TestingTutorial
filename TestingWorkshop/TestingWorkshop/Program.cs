49efbbfdc1bf2e929153b73f8836ede90c2f7862
BRAJAN1984\bpiet <blazej.pietrzyk@gmail.com>
2018-04-03 20:57:54 +0200

    Testing workshop - core

---
+++ TestingWorkshop/TestingWorkshop/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingWorkshop
+{
+    public class doubleDigit
+    {
+        public int first;
+        public int second;
+
+        public int fullNo
+        {
+            get
+            {
+                return int.Parse(string.Format("{0}{1}", first, second));
+            }
+        }
+    }
+
+    public class hour24
+    {
+        public doubleDigit hour;
+        public doubleDigit minutes;
+
+        public doubleDigit seconds;
+
+        public override string ToString()
+        {
+            return new TimeSpan(hour.fullNo, minutes.fullNo, seconds == null ? 0 : seconds.fullNo).ToString();
+        }
+    }
+
+    public class Solution
+    {
+        public string solution(int A, int B, int C, int D, int E, int F)
+        {
+            var digits = new List<int>();
+            digits.Add(A);
+            digits.Add(B);
+            digits.Add(C);
+            digits.Add(D);
+            digits.Add(E);
+            digits.Add(F);
+
+            var correctHour = new List<int>();
+
+            string result = "NOT POSSIBLE";
+
+            var hours = new List<hour24>();
+
+            for (int first = 0; first < 6; first++)
+            {
+                var testDigits = digits.ToList();
+
+                int firstNo = testDigits[first];
+                testDigits.Remove(firstNo);
+
+                foreach (var secondNo in testDigits)
+                {
+                    var hour = new doubleDigit() { first = firstNo, second = secondNo };
+
+                    if (validateHour(hour))
+                    {
+                        hours.Add(new hour24 { hour = hour });
+                    }
+                }
+            }
+
+            var hoursWithMinutes = new List<hour24>();
+
+            foreach (var hour in hours)
+            {
+                var testDigits = digits.ToList();
+                testDigits.Remove(hour.hour.first);
+                testDigits.Remove(hour.hour.second);
+
+                for (int first = 0; first < testDigits.Count(); first++)
+                {
+                    var digitsForNext = testDigits.ToList();
+
+                    int firstNo = digitsForNext[first];
+                    digitsForNext.Remove(firstNo);
+
+                    foreach (var secondNo in digitsForNext)
+                    {
+
+                        var minute = new doubleDigit() { first = firstNo, second = secondNo };
+                        var hourcpy = new hour24
+                        {
+                            hour = hour.hour,
+                            minutes = hour.minutes,
+                            seconds = hour.seconds
+                        };
+                        if (validateMinSec(minute))
+                        {
+                            hourcpy.minutes = minute;
+                            hoursWithMinutes.Add(hourcpy);
+                        }
+                    }
+                }
+            }
+
+            var correctHours = new List<hour24>();
+
+            foreach (var hour in hoursWithMinutes)
+            {
+                var testDigits = digits.ToList();
+                testDigits.Remove(hour.hour.first);
+                testDigits.Remove(hour.hour.second);
+                testDigits.Remove(hour.minutes.first);
+                testDigits.Remove(hour.minutes.second);
+
+                for (int first = 0; first < testDigits.Count(); first++)
+                {
+                    var digitsForNext = testDigits.ToList();
+
+                    int firstNo = digitsForNext[first];
+                    digitsForNext.Remove(firstNo);
+
+                    foreach (var secondNo in digitsForNext)
+                    {
+
+                        var seconds = new doubleDigit() { first = firstNo, second = secondNo };
+
+                        if (validateMinSec(seconds))
+                        {
+
+                            var hourcpy = new hour24
+                            {
+                                hour = hour.hour,
+                                minutes = hour.minutes,
+                                seconds = hour.seconds
+                            };
+                            hourcpy.seconds = seconds;
+                            correctHours.Add(hourcpy);
+                        }
+                    }
+                }
+            }
+
+            if (correctHours.Count() > 0)
+            {
+                var time = correctHours.Select(h => new TimeSpan(h.hour.fullNo, h.minutes.fullNo, h.seconds.fullNo)).Min();
+
+                result = time.ToString();
+            }
+
+            return result;
+        }
+
+        static bool validateHour(doubleDigit hour)
+        {
+            return hour.fullNo >= 0 && hour.fullNo <= 24;
+        }
+
+        static bool validateMinSec(doubleDigit hour)
+        {
+            return hour.fullNo >= 0 && hour.fullNo <= 60;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proc = new Solution();
+
+            var hour = proc.solution(1, 8, 3, 2, 6, 4);
+
+            Console.WriteLine(hour);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestingWorkshop/TestingWorkshopTests/ExplorationTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingWorkshop;
+
+namespace TestingWorkshopTests
+{
+    public class ExplorationTests
+    {
+        Solution _solutionImpl = new Solution();
+
+        [Theory]
+        [TestCase(1, 8, 3, 2, 6, 4, "12:36:48")]
+        [TestCase(4, 4, 4, 4, 4, 4, "NOT POSSIBLE")]
+        [TestCase(1, 9, 3, 5, 6, 8, "16:38:59")]
+        [TestCase(4, 4, 3, 9, 6, 2, "23:46:49")]
+        [TestCase(0, 2, 1, 3, 1, 2, "01:12:23")]
+        [TestCase(1, 8, 0, 2, 0, 4, "00:12:48")]
+        public void SolutionExplorationTest(int A, int B, int C, int D, int E, int F, string expectedValue)
+        {
+            var result = _solutionImpl.solution(A, B, C, D, E, F);
+
+            result.Should().Be(expectedValue);
+        }
+    }
+}
